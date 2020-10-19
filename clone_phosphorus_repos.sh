377493ca46197fb368928a4a056328de67a7f22e
brian lai <brian.lai@phosphorus.com>
2020-10-19 11:17:33 -0400

    add clone_phosphorus_repo shell script

---
+++ clone_phosphorus_repos.sh
@@ -0,0 +1,12 @@
+git clone git@github.com:p15/phosphorus-api-ruby-client.git
+git clone git@github.com:p15/phosphorus-atomic.git
+git clone git@github.com:p15/phosphorus-engine.git
+git clone git@github.com:p15/phosphorus-mgmt.git
+git clone git@github.com:p15/phosphorus-one.git
+git clone git@github.com:p15/phosphorus-practice.git
+git clone git@github.com:p15/pipeline-scripts.git
+git clone git@github.com:p15/recombine-api.git
+git clone git@github.com:p15/recombine-mgmt.git
+git clone git@github.com:p15/recombine-migrations.git
+git clone git@github.com:p15/recombine-rails-suite.git
+git clone git@github.com:p15/workflow-utils.git
